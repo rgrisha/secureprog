1393f8a1950011aa2fb881bdbbc61cba832853a4
Rolandas Griskevicius <rol.griskevicius@gmail.com>
2017-09-25 13:19:19 +0300

    fix setup

--- tasks/task1/setup.sh
+++ tasks/task1/setup.sh
@@ -3,8 +3,10 @@ function setup_app {
   mkdir ~/app$1
   cd ~/app$1
   echo "Downloading app $1"
-  wget https://github.com/rgrisha/secureprog/tree/master/tasks/task1/apps/app$1
+  wget https://rawgit.com/rgrisha/secureprog/master/tasks/task$1/apps/app$1
 }
+
+cd ~
 
 for i in `seq 1 4`; do
   setup_app $i
